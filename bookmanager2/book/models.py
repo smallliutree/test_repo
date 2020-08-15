01d15311bc5e217cafb9b26717442dd76053d457
l <254993041@qq.com>
2020-08-15 16:45:18 +0800

    上午

--- bookmanager2/book/models.py
+++ bookmanager2/book/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10, verbose_name='书籍名称')
+    pub_date = models.DateField(null=True)
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '图书'
+
+    def __str__(self):
+        return self.name
+
+
+class PeopleInfo(models.Model):
+    GENDER_CHOICE = (
+        (0, 'male'),
+        (1, 'female')
+    )
+    name = models.CharField(max_length=10)
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=0)
+    description = models.CharField(max_length=100)
+    is_delete = models.BooleanField(default=False)
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'peopleinfo'
+
+    def __str__(self):
+        return self.name
--- bookmanager2/bookmanager2/settings.py
+++ bookmanager2/bookmanager2/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bookmanager2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'NAME': 'book',
     }
 }
 
@@ -104,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
