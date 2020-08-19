be0169419e8897c52a11b31e095cc8d68a5c5a37
l <254993041@qq.com>
2020-08-19 09:56:14 +0800

    中间件

--- bookmanager3/bookmanager3/settings.py
+++ bookmanager3/bookmanager3/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'book.middleware.TestMiddleWare',
 ]
 
 ROOT_URLCONF = 'bookmanager3.urls'
